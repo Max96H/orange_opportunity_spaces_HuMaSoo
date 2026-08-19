b32d71df619ae56f7291221a914cd8e0833e65f9
patoobyte <ake.leesy@gmail.com>
2026-08-19 23:51:14 +0200

    wip: placeholders for switch to db from json

--- src/frontend/data_loader.py
+++ src/frontend/data_loader.py
@@ -1,12 +1,13 @@
 import base64
 import json
+import sqlite3
 from pathlib import Path
 
 import streamlit as st
 
 
 @st.cache_data
-def load_opportunity_spaces(path: Path) -> list[dict]:
+def load_opportunity_spaces(path: Path) -> list[dict]: #replace to load db instead of json
     with path.open("r", encoding="utf-8") as file:
         data = json.load(file)
     return data.get("opportunity_space", [])
